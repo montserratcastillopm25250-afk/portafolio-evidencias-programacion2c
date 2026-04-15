23b06eb4976bbbd3aae561d7a8b245ddddab9ae2
Kelly Cruz <kelly.cruz_pm225239@cecyte.edu.mx>
2026-04-15 07:40:24 -0600

    Se agregan los ejercicios de listas en python

---
+++ ejercicios/kellycruz/parcial2/listas2.py
@@ -0,0 +1,6 @@
+"""
+Estas programando el sistema de una pequeña tienda. Debes manipular el inventario de productos disponibles.
+1. Inicia una lista de productos que contenga:"leche","pan","huevos",manzanas"
+2. Te ha llegado un camion con nuevos productos:"Arroz","Frijol" y "Aceite". Agregando todos a la lista usando un solo metodo
+3. Te das cuenta que el "pan" esta vencido.Encuentra el indice del "pan" usando .index 
+"""
